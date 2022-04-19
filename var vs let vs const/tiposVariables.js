0de92664ae955d991ba44194cd67a1976e5ca82d
Perez Chavez <v853681@global.ual.com>
2022-04-19 13:31:22 -0500

    var vs let vs const examples

---
+++ var vs let vs const/tiposVariables.js
@@ -0,0 +1,45 @@
+//Var
+
+//console.log(titulo);
+var titulo = "Suscribete";
+//console.log(titulo);
+var titulo = 10;
+//console.log(titulo);
+
+var titulo = "Activa la campanita";
+
+
+function obtenerTitulo(){
+    
+    //console.log(titulo);
+    var titulo= "Suscribete";
+}
+
+obtenerTitulo();
+//let
+//console.log(tituloLet);
+let tituloLet = "PMFOX technologies";
+
+tituloLet = 10;
+//console.log(tituloLet);
+
+function obtenerTituloLet(){
+    let titulo2 = "Suscribete";
+    //console.log(tituloLet);
+    //console.log(titulo2);
+}
+obtenerTituloLet();
+//console.log(tituloLet);
+
+//const
+const tituloConst = "PMFOX technologies";
+//tituloConst = 10;
+
+function obtenerTituloConst(){
+    const tituloConst2 = "Suscribete";
+    console.log(tituloConst);
+    console.log(tituloConst2);
+}
+
+obtenerTituloConst();
+console.log(tituloConst);
