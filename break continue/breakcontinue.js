e05c018b9d7f61444bd201753d86656539116d72
Marco Perez Chavez <v-marcoper@microsoft.com>
2022-11-04 07:16:13 -0700

    added break continue

---
+++ break continue/breakcontinue.js
@@ -0,0 +1,17 @@
+for (let i=0; i<10; i++) {
+
+    // CONTINUE
+    if( i == 2 || i == 5){
+        continue;
+    }
+
+    // BREAK
+    if( i == 7){
+        break;
+    }
+
+    
+
+    console.log(i);
+}
+console.log("suscribete");
